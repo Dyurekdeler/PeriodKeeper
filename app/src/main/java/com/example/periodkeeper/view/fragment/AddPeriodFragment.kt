4ae4fb3ebc7b4119580694db7c2238b42d7ee54e
dyurekdeler <dyurekdeler@live.com>
2021-06-28 23:59:48 +0300

    started hilt

--- app/src/main/java/com/example/periodkeeper/view/fragment/AddPeriodFragment.kt
+++ app/src/main/java/com/example/periodkeeper/view/fragment/AddPeriodFragment.kt
@@ -5,19 +5,20 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.activityViewModels
+import androidx.room.Room
 import com.example.gotraveling.Dao.PeriodDao
 import com.example.gotraveling.Database.AppDatabase
 import com.example.periodkeeper.R
 import com.example.periodkeeper.model.Period
+import com.example.periodkeeper.view.activity.MainActivity
 import com.example.periodkeeper.viewmodel.AddPeriodViewModel
 import kotlinx.android.synthetic.main.fragment_add_period.*
 import java.text.SimpleDateFormat
 import java.util.*
 
 class AddPeriodFragment : Fragment() {
-
-    private val addPeriodFragmentViewModel: AddPeriodViewModel by activityViewModels()
 
     private var db: AppDatabase? = null
     private var periodDao: PeriodDao? = null
@@ -40,14 +41,20 @@ class AddPeriodFragment : Fragment() {
         val currentDate = sdf.format(Date())
 
         addPeriodTodayBtn.setOnClickListener {
-
             periodDao?.insertPeriod(Period(null, currentDate))
+            Toast.makeText(context, "Added today period: " + currentDate.toString(), Toast.LENGTH_LONG).show()
         }
 
         addPeriodCustomBtn.setOnClickListener {
             val dateGiven = periodDate.text.toString()
-            if(dateGiven.isNotEmpty())
-                periodDao?.insertPeriod(Period(null, dateGiven ))
+            if(dateGiven.isNotEmpty()) {
+                periodDao?.insertPeriod(Period(null, dateGiven))
+                Toast.makeText(
+                    context,
+                    "Added custom period: " + dateGiven,
+                    Toast.LENGTH_LONG
+                ).show()
+            }
         }
     }
 
