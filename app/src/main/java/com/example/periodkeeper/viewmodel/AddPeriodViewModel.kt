a4cfaeebd0e119d413a3b10c837fac7ad4e87f38
dyurekdeler <dyurekdeler@live.com>
2021-06-29 00:00:07 +0300

    started hilt

--- app/src/main/java/com/example/periodkeeper/viewmodel/AddPeriodViewModel.kt
+++ app/src/main/java/com/example/periodkeeper/viewmodel/AddPeriodViewModel.kt
@@ -1,10 +1,12 @@
 package com.example.periodkeeper.viewmodel
 
+import androidx.core.content.ContentProviderCompat.requireContext
 import androidx.lifecycle.ViewModel
 import com.example.gotraveling.Dao.PeriodDao
 import com.example.gotraveling.Database.AppDatabase
+import com.example.periodkeeper.model.Period
+import kotlinx.android.synthetic.main.fragment_add_period.*
 
-class AddPeriodViewModel : ViewModel() {
-    private var db: AppDatabase? = null
-    private var periodDao: PeriodDao? = null
+class AddPeriodViewModel() : ViewModel() {
+
 }
